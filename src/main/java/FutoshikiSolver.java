6f6f97f510d66b3b82ccba404d031ef55d1fa71e
Jadson José Monteiro Oliveira <jadsonjjmo@gmail.com>
2018-04-26 17:40:14 -0300

    Refactor the code

--- src/main/java/FutoshikiSolver.java
+++ src/main/java/FutoshikiSolver.java
@@ -16,28 +16,22 @@ public class FutoshikiSolver {
     private static int breakPoint;
     private static int dimension;
 
-    public static void main(String[] args) throws IOException {
-
-        //System.setOut(new PrintStream("output.out"));
+    public static void main(final String[] args) throws IOException {
 
         final String inputPath;
         final String outputPath;
         int heuristicType = 0;
-        boolean getSolution = true;
 
         try {
             inputPath = args[0];
             outputPath = args[1];
             heuristicType = Integer.parseInt(args[2]);
-            getSolution = Boolean.parseBoolean(args[3]);
             bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(inputPath)));
             bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath)));
-        } catch (Exception e) {
-            System.err.println("Please use: [0] inputPath, [1] outputPath, [2] heuristic type, [3] getSolution\n" + e.getMessage());
+        } catch (final Exception e) {
+            System.err.println("Please use: [0] inputPath, [1] outputPath, [2] heuristic type\n" + e.getMessage());
             System.exit(-1);
         }
-
-        stringBuilder.append("test,heuristicType,dimension,constraints,operations,checkMrv,solutionFound,second\n");
 
         final int quantityOfTests = Integer.parseInt(bufferedReader.readLine());
 
@@ -65,8 +59,6 @@ public class FutoshikiSolver {
                 constraints.put(l2 + c2, list2);
             }
 
-            int quantityOfEmptyVariables = 0;
-
             for (int j = 0; j < dimension; j++) {
                 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
                 for (int k = 0; k < dimension; k++) {
@@ -79,69 +71,37 @@ public class FutoshikiSolver {
                         board[dimension][j] |= (1 << value);
                         //dimension + 1 = column
                         board[dimension + 1][k] |= (1 << value);
-                    } else{
-                        quantityOfEmptyVariables ++;
                     }
+
                 }
             }
 
-            int result[][] = null;
+            breakPoint = 0;
 
-            for (int h = 3; h <= 3; h++) {
+            final int[][] result = recursiveBacktracking(board, heuristicType);
 
-                heuristicType = h;
-                double totalTime = 0;
-                int iterations = 5;
+            stringBuilder.append(i + 1).append("\n");
+            printBoard(result);
 
-                for (int iter = 0; iter < iterations; iter++) {
-
-
-                    final int tempBoard[][] = new int[dimension + 2][dimension];
-
-                    for (int q = 0; q < dimension + 2; q++) {
-                        for (int w = 0; w < dimension; w++) {
-                            tempBoard[q][w] = board[q][w];
-                        }
-                    }
-
-                    final double initialTime = System.currentTimeMillis();
-
-                    breakPoint = 0;
-                    result = backtrackingSearch(heuristicType, tempBoard);
-
-                    if (getSolution) {
-                        stringBuilder.append(i + 1).append("\n");
-                        printBoard(result);
-                    }
-                    //printBoard(backtrackingSearch(heuristicType, board));
-                    //System.out.println(breakPoint);
-                    //System.out.println((System.currentTimeMillis() - initialTime) / 1000);
-
-
-                    //Read break line after each test case, except the last of them
-                    final double endTime = System.currentTimeMillis();
-                    totalTime += (endTime - initialTime);
-                }
-
-                stringBuilder.append(i + 1).append(",").append(heuristicType).append(",")
-                        .append(dimension).append(",").append(quantityOfConstraints).append(",").append(breakPoint)
-                        .append(",").append(breakPoint == quantityOfEmptyVariables).append(",").append(result != null).append(",").append((totalTime / iterations) / 1000).append("\n");
-            }
             if (i + 1 < quantityOfTests) {
                 bufferedReader.readLine();
             }
         }
-
         bufferedWriter.write(stringBuilder.toString());
         bufferedWriter.flush();
         bufferedWriter.close();
     }
 
-    private static int[][] backtrackingSearch(final int heuristicType, int[][] board) {
-        return recursiveBacktracking(board, heuristicType);
-    }
 
-    private static int[][] recursiveBacktracking(int[][] board, int heuristicType) {
+    /**
+     * This is the main method for backtracking approach
+     *
+     * @param board         A initial or partial board
+     * @param heuristicType The heuristic type to be used
+     * @return A matrix with the possible solution
+     */
+    private static int[][] recursiveBacktracking(final int[][] board, final int heuristicType) {
+
         if (isSolution(board)) {
             return board;
         }
@@ -150,6 +110,8 @@ public class FutoshikiSolver {
 
         final int row = (int) variableAndOrderDomainValues[0];
         final int column = (int) variableAndOrderDomainValues[1];
+
+        //noinspection unchecked
         final ArrayList<Integer> orderDomainValues = (ArrayList<Integer>) variableAndOrderDomainValues[2];
 
         for (final int value : orderDomainValues) {
@@ -179,75 +141,55 @@ public class FutoshikiSolver {
 
     }
 
+
     /**
-     * This method will compare if the board has any empty space or if is complete.
+     * This method will redirect the getting of variables and domain values following the heuristic informed
+     * on parameters.
      *
-     * @param board A board with the possible solution
-     * @return true case the board is complete or false otherwise
+     * @param board         A matrix with the current board state
+     * @param heuristicType The method to be used (BT, BT + FC, BT + FC + MRV, ...)
+     * @return An object containing the row and column (variable information) and the domain valid values for that
+     * specific variable.
      */
-    private static boolean isSolution(int[][] board) {
+    private static Object[] getVariableAndOrderDomainValues(final int[][] board, final int heuristicType) {
 
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
+        final Object[] result = new Object[3];
 
-                final int number = board[i][j];
-
-                if (number == 0) {
-                    return false;
-                }
-            }
+        switch (heuristicType) {
+            case 0:
+                simpleUnassignedVariable(board, result);
+                simpleDomainValues(board, result);
+                break;
+            case 1:
+                simpleUnassignedVariable(board, result);
+                simpleDomainValues(board, result);
+                forwardChecking(board, result);
+                break;
+            case 2:
+                mrvForwardChecking(board, result);
+                simpleDomainValues(board, result);
+                break;
+            case 3:
+                mrvForwardChecking02(board, result);
+                simpleDomainValues(board, result);
+                break;
+            case 4:
+                mrvForwardCheckingDegree02(board, result);
+                simpleDomainValues(board, result);
+                break;
         }
 
-        return true;
+        return result;
     }
 
-    private static boolean breakConstraint(int[][] board, int row, int column, int value) {
 
-        final List<String> listOfConstraints = constraints.get((row + 1) + "" + (column + 1));
-
-        if (listOfConstraints != null) {
-            for (final String constraint : listOfConstraints) {
-                final char operator = constraint.charAt(0);
-                final int row2 = Character.getNumericValue(constraint.charAt(1)) - 1;
-                final int column2 = Character.getNumericValue(constraint.charAt(2)) - 1;
-                final int value2 = board[row2][column2];
-
-                if (operator == '<' && (value2 != 0 && value2 < value)) {
-                    return true;
-                } else if (operator == '>' && (value2 != 0 && value2 > value)) {
-                    return true;
-                }
-
-            }
-        }
-
-        return false;
-    }
-
-    private static void printBoard(int[][] board) {
-
-        if (board == null) {
-            System.out.println("No solution found!");
-            stringBuilder.append("No solution found!\n");
-        } else {
-            for (int i = 0; i < dimension; i++) {
-                for (int j = 0; j < dimension; j++) {
-                    System.out.print(board[i][j]);
-                    stringBuilder.append(board[i][j]);
-                    if (j + 1 < dimension) {
-                        System.out.print(" ");
-                        stringBuilder.append(" ");
-                    } else {
-                        System.out.println();
-                        stringBuilder.append("\n");
-                    }
-                }
-            }
-        }
-
-    }
-
-    private static void simpleUnassignedVariable(int[][] board, Object[] result) {
+    /**
+     * Return the first empty variable
+     *
+     * @param board  A matrix with the current board state
+     * @param result An object with [0] and [1] positions filled (row and column information)
+     */
+    private static void simpleUnassignedVariable(final int[][] board, final Object[] result) {
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
                 if (board[i][j] == 0) {
@@ -259,7 +201,15 @@ public class FutoshikiSolver {
         }
     }
 
-    private static boolean forwardChecking(int[][] board, Object[] result) {
+
+    /**
+     * This method checks if there is any variable with no valid values available
+     *
+     * @param board  A matrix with the current board state
+     * @param result An object with the variable and domain values information
+     * @return true if there is any variable with no valid values and false otherwise
+     */
+    private static boolean forwardChecking(final int[][] board, final Object[] result) {
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
 
@@ -279,7 +229,14 @@ public class FutoshikiSolver {
         return true;
     }
 
-    private static void mrvForwardChecking(int[][] board, Object[] result) {
+
+    /**
+     * This method follows the default MRV rules and return the variable with minimum conflicting values
+     *
+     * @param board  A matrix with the current board state
+     * @param result An object with [0] and [1] positions filled (row and column information)
+     */
+    private static void mrvForwardChecking(final int[][] board, final Object[] result) {
         int minValidValues = Integer.MAX_VALUE;
 
         for (int i = 0; i < dimension; i++) {
@@ -307,7 +264,14 @@ public class FutoshikiSolver {
         }
     }
 
-    private static void mrvForwardChecking02(int[][] board, Object[] result) {
+
+    /**
+     * This method will return the variable with minimum conflicting values (considering row and column)
+     *
+     * @param board  A matrix with the current board state
+     * @param result An object with [0] and [1] positions filled (row and column information)
+     */
+    private static void mrvForwardChecking02(final int[][] board, final Object[] result) {
         int minValidValues = Integer.MAX_VALUE;
 
         for (int i = 0; i < dimension; i++) {
@@ -332,7 +296,14 @@ public class FutoshikiSolver {
         }
     }
 
-    private static void mrvForwardCheckingDegree02(int[][] board, Object[] result) {
+
+    /**
+     * This method will return the variable with minimum valid values, with degree heuristic tie breaker.
+     *
+     * @param board  A matrix with the current board state
+     * @param result An object with the [0] and [1] positions filled (row and column information)
+     */
+    private static void mrvForwardCheckingDegree02(final int[][] board, final Object[] result) {
         int minValidValues = Integer.MAX_VALUE;
         int maxDegree = 0;
 
@@ -367,44 +338,20 @@ public class FutoshikiSolver {
         }
     }
 
-    private static Object[] getVariableAndOrderDomainValues(int[][] board, int heuristicType) {
 
-        final Object[] result = new Object[3];
-
-        switch (heuristicType) {
-            case 0:
-                simpleUnassignedVariable(board, result);
-                simpleDomainValues(board, result);
-                break;
-            case 1:
-                simpleUnassignedVariable(board, result);
-                simpleDomainValues(board, result);
-                forwardChecking(board, result);
-                break;
-            case 2:
-                mrvForwardChecking(board, result);
-                simpleDomainValues(board, result);
-                break;
-            case 3:
-                mrvForwardChecking02(board, result);
-                simpleDomainValues(board, result);
-                break;
-            case 4:
-                mrvForwardCheckingDegree02(board, result);
-                simpleDomainValues(board, result);
-                break;
-        }
-
-        return result;
-    }
-
-    private static void simpleDomainValues(int[][] board, Object[] result) {
+    /**
+     * This method will fill the domain valid values fora a specific variable. It will fill the result[2] value.
+     *
+     * @param board  A matrix with the current board state
+     * @param result Result is a variable that contains the row, column and domain values information
+     */
+    private static void simpleDomainValues(final int[][] board, final Object[] result) {
         final ArrayList<Integer> listOfValidValues = new ArrayList<>();
 
-        int row = (int) result[0];
-        int column = (int) result[1];
+        final int row = (int) result[0];
+        final int column = (int) result[1];
 
-        int validValues = board[dimension][row] | board[dimension + 1][column];
+        final int validValues = board[dimension][row] | board[dimension + 1][column];
 
         for (int value = dimension; value >= 1; value--) {
             if (((1 << value) | validValues) != validValues) {
@@ -417,10 +364,82 @@ public class FutoshikiSolver {
         result[2] = listOfValidValues;
     }
 
-    private static int getDegree(int row, int column) {
+
+    /**
+     * This method will compare if the board has any empty space or if is complete.
+     *
+     * @param board A board with the possible solution
+     * @return true case the board is complete or false otherwise
+     */
+    private static boolean isSolution(final int[][] board) {
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+
+                final int number = board[i][j];
+
+                if (number == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * This method returns true if when assigning some constraint is broken or true otherwise.
+     *
+     * @param board  The current board state
+     * @param row    The row to be analysed
+     * @param column The column to be analysed
+     * @param value  The value to be analysed
+     * @return true if constraints are being respected or false, otherwise.
+     */
+    private static boolean breakConstraint(final int[][] board, final int row, final int column, final int value) {
+
+        final List<String> listOfConstraints = constraints.get((row + 1) + "" + (column + 1));
+
+        if (listOfConstraints != null) {
+            for (final String constraint : listOfConstraints) {
+                final char operator = constraint.charAt(0);
+                final int row2 = Character.getNumericValue(constraint.charAt(1)) - 1;
+                final int column2 = Character.getNumericValue(constraint.charAt(2)) - 1;
+                final int value2 = board[row2][column2];
+
+                if (operator == '<' && (value2 != 0 && value2 < value)) {
+                    return true;
+                } else if (operator == '>' && (value2 != 0 && value2 > value)) {
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * This method will return the quantity of degrees for a specific variable. The degrees follow the constraint of
+     * smaller and larger value for a specific variable.
+     *
+     * @param row    The row of the variable to be analysed.
+     * @param column The column of the variable to be analysed.
+     * @return
+     */
+    private static int getDegree(final int row, final int column) {
         return constraints.getOrDefault((row + "" + column), new ArrayList<>()).size();
     }
 
+
+    /**
+     * This method will return the quantity of valid values (respecting only the exclusivity constraint)
+     *
+     * @param n The or bit operation between the row and column valid values
+     * @return The quantity of valid values respecting the constraints
+     */
     private static int getValidValues(int n) {
         //remove the first bit, because it always be zero
         int count = 0;
@@ -435,8 +454,19 @@ public class FutoshikiSolver {
         return dimension - count;
     }
 
-    private static int getValidValues02(int[][] board, int row, int column, int n) {
-        //remove the first bit, because it always be zero
+
+    /**
+     * This method will return the quantity of valid values to the specific domain, following all the constraints of the
+     * problem.
+     *
+     * @param board  A matrix with the current board state
+     * @param row    The row to be analysed
+     * @param column The column to be analysed
+     * @param n      The or bit operation between the row and column
+     * @return The quantity of valid values respecting all constraints
+     */
+    private static int getValidValues02(final int[][] board, final int row, final int column, final int n) {
+        //remove the first bit, because it always be zero (1<<1 = 00010 for a 4x4 board)
         int count = 0;
 
         for (int i = 1; i <= dimension; i++) {
@@ -450,6 +480,31 @@ public class FutoshikiSolver {
         }
 
         return count;
+    }
+
+
+    /**
+     * Prints the board or a fault message if the board is null
+     *
+     * @param board A board with the possible solution
+     */
+    private static void printBoard(final int[][] board) {
+
+        if (board == null) {
+            stringBuilder.append(breakPoint >= 1e6 ? "Numero de atribuicoes excede limite maximo\n" : "No solution found\n");
+        } else {
+            for (int i = 0; i < dimension; i++) {
+                for (int j = 0; j < dimension; j++) {
+                    stringBuilder.append(board[i][j]);
+                    if (j + 1 < dimension) {
+                        stringBuilder.append(" ");
+                    } else {
+                        stringBuilder.append("\n");
+                    }
+                }
+            }
+        }
+
     }
 
 }
